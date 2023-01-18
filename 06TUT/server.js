2064434215b71c50d944fe17cd2a345740d97d19
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-01-18 20:37:37 +0100

    Practise using express framework to handle routing

--- 06TUT/server.js
+++ 06TUT/server.js
@@ -1,103 +1,49 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-//const { parse } = require('path');
-
-class Emitter extends EventEmitter { };
-
-// Initialize the object
-const myEmitter = new Emitter();
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const rawData = await fsPromises.readFile(
-            filePath,
-            !contentType.includes('image') ? 'utf8' : ''
-        );
-        const data = contentType === 'application/json'
-            ? JSON.parse(rawData) : rawData;
-        response.writeHead(
-            filePath.includes('404.html') ? 400 : 200,
-            { 'Content-Type': contentType }
-        );
-        response.end(
-            contentType === 'application/json' ? JSON.stringify(data) : data
-        );
-    } catch (error) {
-        console.log(error);
-        myEmitter.emit('log', `${error.name}: ${error.message}`, 'errLog.html');
-        response.statusCode = 500;
-        response.end();
-    }
+app.get('^/$|/index(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname }); One way to serve a page
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, 'new-page.html'); // 302 by default
+});
+
+// Router handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next();
+}, (req, res) => {
+    res.send('Hello world!');
+})
+
+const one = (req, res, next) => {
+    console.log('One');
+    next();
 }
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.html');
+const two = (req, res, next) => {
+    console.log('Two');
+    next()
+}
 
-    const extension = path.extname(req.url);
-    let contentType;
+const three = (req, res) => {
+    console.log('Three');
+    res.send('Finished!');
+}
 
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css'
-            break;
-        case '.js':
-            contentType = 'text/javascript'
-            break;
-        case '.json':
-            contentType = 'application/json'
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg'
-            break;
-        case '.png':
-            contentType = 'image/png'
-            break;
-        case '.txt':
-            contentType = 'text/plain'
-            break;
-        default:
-            contentType = 'text/html'
-    }
+app.get('/chain(.html)?', [one, two, three]);
 
-    let filePath =
-        contentType === 'text/html' && req.url === '/'
-            ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/'
-                ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html'
-                    ? path.join(__dirname, 'views', req.url)
-                    : path.join(__dirname, req.url)
-
-    // make .html extension not required in the browser
-    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
-
-    const fileExist = fs.existsSync(filePath);
-
-    if (fileExist) {
-        // Serve the file
-        serveFile(filePath, contentType, res);
-    } else {
-        switch(path.parse(filePath).base) {
-            case 'old-page.html':
-                res.writeHead(301, { 'Location': '/new-page.html'});
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, { 'Location': '/'});
-                res.end();
-                break;
-            default:
-                // serve  404
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-        }
-    }
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
